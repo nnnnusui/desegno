510058f4bd1ff2f0a421512a4115fdfae0dc53c1
nnnnusui <nnnnusui@gmail.com>
2024-06-19 21:10:01 +0000

    difinu `vk.Instanco`[- envolvis VkInstance]

---
+++ src/main/scala/io/github/nnnnusui/desegno/vulkan/Instanco.scala
@@ -0,0 +1,43 @@
+package io.github.nnnnusui.desegno.vulkan
+
+import org.lwjgl.glfw.GLFWVulkan
+import org.lwjgl.system.MemoryStack as MemoroStako
+import org.lwjgl.system.MemoryUtil as MemoraUtilo
+import org.lwjgl.vulkan.VK10
+import org.lwjgl.vulkan.VK13
+import org.lwjgl.vulkan.VkApplicationInfo
+import org.lwjgl.vulkan.VkInstance
+import org.lwjgl.vulkan.VkInstanceCreateInfo
+import scala.util.Using.Releasable as Liberigebla
+
+import io.github.nnnnusui.desegno._tradukita.*
+
+object Instanco:
+  def krei(
+      aplTitolo: Signoĉeno = "Nenio nomo",
+  )(using stako: MemoroStako) =
+    val aplInfo = VkApplicationInfo.calloc(stako)
+    aplInfo.sType(VK10.VK_STRUCTURE_TYPE_APPLICATION_INFO)
+    aplInfo.pApplicationName(MemoraUtilo.memASCII(aplTitolo))
+    aplInfo.applicationVersion(VK10.VK_MAKE_VERSION(1, 0, 0))
+    aplInfo.pEngineName(MemoraUtilo.memASCII("Neniu Motoro"))
+    aplInfo.engineVersion(VK10.VK_MAKE_VERSION(1, 0, 0))
+    aplInfo.apiVersion(VK13.VK_API_VERSION_1_3)
+
+    val kreoInfo = VkInstanceCreateInfo.calloc(stako)
+    kreoInfo.sType(VK10.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
+    kreoInfo.pApplicationInfo(aplInfo)
+    kreoInfo.ppEnabledExtensionNames(GLFWVulkan.glfwGetRequiredInstanceExtensions())
+
+    val instancoMontrilo = stako.mallocPointer(1)
+    if VK10.vkCreateInstance(kreoInfo, null, instancoMontrilo) != VK10.VK_SUCCESS then
+      throw RuntimeException("Malsukcesis krei la Vulkan-instancon")
+    val instanco = VkInstance(instancoMontrilo.get(0), kreoInfo)
+    new Instanco(instanco)
+  given Liberigebla[Instanco] = (instanco) => instanco.detrui();
+
+class Instanco(
+    val kruda: VkInstance,
+):
+  def detrui(): Unuo =
+    VK10.vkDestroyInstance(kruda, null)
--- src/main/scala/io/github/nnnnusui/desegno/Ĉefa.scala
+++ src/main/scala/io/github/nnnnusui/desegno/Ĉefa.scala
@@ -1,12 +1,20 @@
 package io.github.nnnnusui.desegno
 
+import org.lwjgl.system.MemoryStack.stackPush as stakoPuŝi
+import org.lwjgl.system.MemoryStack as MemoroStako
 import scala.main as ĉefa
+import scala.util.Using.Manager as UzantaManaĝero
 import scala.util.Using as Uzanta
 
 import io.github.nnnnusui.desegno._tradukita.*
+import io.github.nnnnusui.desegno.vulkan as vk
 
 @ĉefa def ĉefa =
   presiln("Scala skribita en esperanto.")
-  Uzanta(Fenestro.krei()): fenestro =>
+  val aplTitolo = "desegno"
+  UzantaManaĝero: uzi =>
+    given MemoroStako = uzi(stakoPuŝi())
+    val vkInstanco = uzi(vk.Instanco.krei(aplTitolo = aplTitolo))
+    val fenestro = uzi(Fenestro.krei(titolo = aplTitolo))
     fenestro.bildigaBuklo: _ =>
       ()
